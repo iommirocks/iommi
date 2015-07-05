90a819edace29828d2e55edd1d1aac3df624b28e
Anders Hovmöller <boxed@killingar.net>
2015-07-05 21:28:34 +0200

    README sync test should only run under the "make docs" command

--- test_readme.py
+++ test_readme.py
@@ -1,8 +1,11 @@
 def test_readme():
-    a = open('docs/_build/html/index.html').read()
-    b = open('docs/_build/html/readme_test.html').read()
+    try:
+        a = open('docs/_build/html/index.html').read()
+        b = open('docs/_build/html/readme_test.html').read()
     
-    def cut_and_normalize(foo):
-        return foo[foo.find('<body'):foo.find('<footer>')].replace('class="code python highlight-python"', 'class="n"').replace('class="highlight-python"', 'class="n"').replace('href="index.html"', 'href="#"').replace('href="_sources/readme_test.txt"', 'href="_sources/index.txt"')
+        def cut_and_normalize(foo):
+            return foo[foo.find('<body'):foo.find('<footer>')].replace('class="code python highlight-python"', 'class="n"').replace('class="highlight-python"', 'class="n"').replace('href="index.html"', 'href="#"').replace('href="_sources/readme_test.txt"', 'href="_sources/index.txt"')
 
-    assert cut_and_normalize(a) == cut_and_normalize(b)
+        assert cut_and_normalize(a) == cut_and_normalize(b)
+    except IOError:
+        pass
