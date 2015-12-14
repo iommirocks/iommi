6f2674cbb912a99327543fe81b327e83bca5d841
Johan Lübcke <johan.lubcke@trioptima.com>
2015-12-14 13:26:52 +0100

    Cleanup tox config

--- setup.py
+++ setup.py
@@ -8,21 +8,6 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
-class PyTest(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import sys, subprocess
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
-
-
 def read_reqs(name):
     with open(os.path.join(os.path.dirname(__file__), name)) as f:
         return [line for line in f.read().split('\n') if line and not line.strip().startswith('#')]
@@ -64,5 +49,4 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
-    cmdclass={'test': PyTest},
 )
