8c2056521707523e9629163e8e9088ed3cffc0cf
Johan Lübcke <johan.lubcke@trioptima.com>
2015-12-15 12:09:39 +0100

    Added make target for rolling release

--- setup.py
+++ setup.py
@@ -21,6 +21,49 @@ def read_version():
         raise ValueError("couldn't find version")
 
 
+class Tag(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from subprocess import call
+        version = read_version()
+        errno = call(['git', 'tag', '--annotate', version, '--message', 'Version %s' % version])
+        if errno == 0:
+            print("Added tag for version %s" % version)
+        raise SystemExit(errno)
+
+
+class ReleaseCheck(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from subprocess import check_output
+        tag = check_output(['git', 'describe', '--all', '--exact-match', 'HEAD']).strip()
+        version = read_version()
+        if tag != version:
+            print('Missing %s tag on release' % version)
+            raise SystemExit(1)
+
+        current_branch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip()
+        if current_branch != 'master':
+            print('Only release from master')
+            raise SystemExit(1)
+
+        print("Ok to distribute files")
+
+
 # NB: _don't_ add namespace_packages to setup(), it'll break
 #     everything using imp.find_module
 setup(
@@ -39,15 +82,15 @@ setup(
     zip_safe=False,
     keywords='tri.query',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
     ],
     test_suite='tests',
+    cmdclass={'tag': Tag,
+              'release_check': ReleaseCheck},
 )
