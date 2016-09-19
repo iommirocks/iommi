69fcb4a28d5d6fc82008aa90c7795172e41177fc
Anders Hovmöller <anders.hovmoller@trioptima.com>
2016-09-19 09:59:09 +0200

    Update to tri.query 3.1 and tri.form 4.0

--- lib/tri/table/db_compat.py
+++ lib/tri/table/db_compat.py
@@ -1,4 +1,4 @@
-from tri.declarative import setdefaults
+from tri.declarative import setdefaults_path
 
 
 def setup_db_compat():
@@ -6,6 +6,28 @@ def setup_db_compat():
 
 
 def setup_db_compat_django():
+    def many_to_many_factory(model_field, **kwargs):
+        setdefaults_path(
+            kwargs,
+            choices=model_field.rel.to._default_manager.all(),
+            model=model_field.foreign_related_fields[0].model,
+        )
+        return Column.multi_choice_queryset(model_field=model_field, **kwargs)
+
+    def auto_field_factory(**kwargs):
+        setdefaults_path(
+            kwargs,
+            show=False,
+        )
+        return Column.integer(**kwargs)
+
+    def comma_separated_integer_factory(**kwargs):
+        setdefaults_path(
+            kwargs,
+            show=False,
+        )
+        return Column.comma_separated(parent_field=Column.integer(**kwargs))
+
     from tri.table import Column, register_column_factory
     try:
         # noinspection PyUnresolvedReferences
@@ -15,29 +37,31 @@ def setup_db_compat_django():
         pass
     else:
         # The order here is significant because of inheritance structure. More specific must be below less specific.
-        register_column_factory(CharField, lambda model_field, **kwargs: Column(**kwargs))
-        register_column_factory(URLField, lambda model_field, **kwargs: Column.url(**kwargs))
-        register_column_factory(TimeField, lambda model_field, **kwargs: Column.time(**kwargs))
-        register_column_factory(EmailField, lambda model_field, **kwargs: Column.email(**kwargs))
-        register_column_factory(DecimalField, lambda model_field, **kwargs: Column.decimal(**kwargs))
-        register_column_factory(DateField, lambda model_field, **kwargs: Column.date(**kwargs))
-        register_column_factory(DateTimeField, lambda model_field, **kwargs: Column.datetime(**kwargs))
-        register_column_factory(CommaSeparatedIntegerField, lambda model_field, **kwargs: Column.comma_separated(parent_field=Column.integer(**kwargs)))
-        register_column_factory(BooleanField, lambda model_field, **kwargs: Column.boolean(**kwargs))
-        register_column_factory(TextField, lambda model_field, **kwargs: Column.text(**kwargs))
-        register_column_factory(FloatField, lambda model_field, **kwargs: Column.float(**kwargs))
-        register_column_factory(IntegerField, lambda model_field, **kwargs: Column.integer(**kwargs))
-        register_column_factory(AutoField, lambda model_field, **kwargs: Column.integer(**setdefaults(kwargs, dict(show=False))))
+        register_column_factory(CharField, Column)
+        register_column_factory(URLField, Column.url)
+        register_column_factory(TimeField, Column.time)
+        register_column_factory(EmailField, Column.email)
+        register_column_factory(DecimalField, Column.decimal)
+        register_column_factory(DateField, Column.date)
+        register_column_factory(DateTimeField, Column.datetime)
+        register_column_factory(BooleanField, Column.boolean)
+        register_column_factory(TextField, Column.text)
+        register_column_factory(FloatField, Column.float)
+        register_column_factory(IntegerField, Column.integer)
+
+        register_column_factory(CommaSeparatedIntegerField, comma_separated_integer_factory)
+        register_column_factory(AutoField, auto_field_factory)
         register_column_factory(ManyToOneRel, None)
-        register_column_factory(ManyToManyField, lambda model_field, **kwargs: Column.multi_choice_queryset(**setdefaults(kwargs, dict(choices=model_field.rel.to._default_manager.all()))))
+        register_column_factory(ManyToManyField, many_to_many_factory)
         register_column_factory(ManyToManyRel, None)
         # register_field_factory(ManyToManyRel_Related, None)
 
         def foreign_key_factory(model_field, **kwargs):
-            setdefaults(kwargs, dict(
-                choices=model_field.foreign_related_fields[0].model.objects.all()
-            ))
-            kwargs['model'] = model_field.foreign_related_fields[0].model
-            return Column.choice_queryset(**kwargs)
+            setdefaults_path(
+                kwargs,
+                choices=model_field.foreign_related_fields[0].model.objects.all(),
+                model=model_field.foreign_related_fields[0].model,
+            )
+            return Column.choice_queryset(model_field=model_field, **kwargs)
 
         register_column_factory(ForeignKey, foreign_key_factory)
