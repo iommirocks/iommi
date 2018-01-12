8b42634cbb0d897a36fc625bb3e8d5ad05550ce2
Anders Hovmöller <anders.hovmoller@trioptima.com>
2018-01-12 13:43:41 +0100

    Fixes github #18

--- examples/examples/settings.py
+++ examples/examples/settings.py
@@ -24,6 +24,22 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ALLOWED_HOSTS = []
 
 
--- examples/examples/urls.py
+++ examples/examples/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from examples import views
 
-urlpatterns = patterns('',
-    url(r'^$', 'examples.views.index', name='index'),
-    url(r'^readme_example_1/$', 'examples.views.readme_example_1', name='readme_example_1'),
-    url(r'^readme_example_2/$', 'examples.views.readme_example_2', name='readme_example_2'),
-    url(r'^kitchen_sink/$', 'examples.views.kitchen_sink', name='kitchen_sink'),
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^readme_example_1/$', views.readme_example_1, name='readme_example_1'),
+    url(r'^readme_example_2/$', views.readme_example_2, name='readme_example_2'),
+    url(r'^kitchen_sink/$', views.kitchen_sink, name='kitchen_sink'),
 
-    url(r'^style.css$', 'examples.views.style', name='style'),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^style.css$', views.style, name='style'),
+]
