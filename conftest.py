72aa4a16605827fe8a70653e3ff8c33d2a290d64
Anders Hovmöller <anders.hovmoller@trioptima.com>
2019-01-11 14:37:05 +0100

    Upgrade to pytest 4

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@ import pytest
 
 
 def pytest_runtest_setup(item):
-    django_marker = item.get_marker("django_db") or item.get_marker("django")
+    django_marker = item.get_closest_marker("django_db") or item.get_closest_marker("django")
     if django_marker is not None:
         try:
             import django
         except ImportError:
             pytest.skip("test requires django")
 
-    flask_marker = item.get_marker("flask")
+    flask_marker = item.get_closest_marker("flask")
     if flask_marker is not None:
         try:
             import flask
