f15c99cc08871d9ec3d498229d99bc76f375f829
Anders Hovmöller <anders.hovmoller@trioptima.com>
2019-08-27 10:36:27 +0200

    Updated choice_select2.html to be compatible with select2 4.x Added an example for select2

--- examples/examples/views.py
+++ examples/examples/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 
-from examples.models import Foo
+from .models import Foo, Bar
 from tri_form import Form, Field, Link
 from tri_form.views import create_object, edit_object
+
+for i in range(100 - Foo.objects.count()):
+    Foo.objects.create(name=f'X{i}', a=i, b=True)
 
 
 def index(request):
@@ -16,6 +19,7 @@ def index(request):
         <a href="example_2/">Example 2 create</a><br/>
         <a href="example_3/">Example 3 edit</a><br/>
         <a href="example_4/">Example 4 custom buttons</a><br/>
+        <a href="example_5/">Example 5 automatic AJAX endpoint</a><br/>
         </body></html>""")
 
 
@@ -64,6 +68,20 @@ def example_4(request):
     return edit_object(
         request,
         instance=Foo.objects.all().first(),
+        form__links=[
+            Link.submit(attrs__value='Foo'),
+            Link.submit(attrs__value='Bar'),
+            Link(title='Back to index', attrs__href='/'),
+        ]
+    )
+
+
+def example_5(request):
+    return create_object(
+        request,
+        model=Bar,
+        form__base_template='tri_form/base_select2.html',
+        form__field__b__input_template='tri_form/choice_select2.html',
         form__links=[
             Link.submit(attrs__value='Foo'),
             Link.submit(attrs__value='Bar'),
