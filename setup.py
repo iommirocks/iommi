4aaa44abb89b59f32bc81d66e39d94013925c22c
Johan Lübcke <johan.lubcke@trioptima.com>
2020-01-08 14:41:40 +0100

    Upgrade from python3.6 to python3.7

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     cmdclass={'tag': Tag,
