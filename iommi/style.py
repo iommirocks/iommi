05051d16d71e2af9d254bb096754f6e01ab3773b
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-02-14 09:34:37 +0100

    Fixed semantic ui styling of radio buttons

--- iommi/style.py
+++ iommi/style.py
@@ -254,6 +254,8 @@ semantic_ui_base = Style(
             boolean=dict(
                 template='iommi/form/semantic_ui/row_checkbox.html',
             ),
+            radio__input__template='iommi/form/semantic_ui/radio.html',
+            radio__attrs__class={'grouped fields': True},
         ),
         attrs__class__field=True,
         template='iommi/form/semantic_ui/row.html',
