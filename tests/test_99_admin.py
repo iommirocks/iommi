a724a61207b14d0b27c3662724144528f7ee7941
Johan Lübcke <johan.lubcke@trioptima.com>
2020-02-27 22:08:59 +0100

    Fix tests

--- tests/test_99_admin.py
+++ tests/test_99_admin.py
@@ -1,11 +1,11 @@
 import pytest
 
-from iommi.admin import list_model
+from iommi.admin import Admin
 from .helpers import req
 
 
 @pytest.mark.django_db
 def test_bulk_edit_for_non_unique():
-    p = list_model(request=None, app_name='tests', model_name='adminunique')
+    p = Admin.list(request=None, app_name='tests', model_name='adminunique')
     p = p.bind(request=req('get'))
-    assert [x._name for x in p.columns.values() if x.bulk.include] == ['foo']
+    assert [x._name for x in p.parts.table.children.text.columns.values() if x.bulk.include] == ['foo']
