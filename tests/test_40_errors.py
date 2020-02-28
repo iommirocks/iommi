99b432a2e2977b9deb752c291550034cb3c1faed
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-02-28 09:21:39 +0100

    Better tests for error.py

--- tests/test_40_errors.py
+++ tests/test_40_errors.py
@@ -3,4 +3,11 @@ from iommi.error import Errors
 
 def test_errors_rendering():
     errors = Errors(parent=None, errors={'foo', 'bar'})
+    assert errors
     assert errors.__html__() == '<ul><li>bar</li><li>foo</li></ul>'
+
+
+def test_errors_empty_rendering():
+    errors = Errors(parent=None, errors=set())
+    assert not errors
+    assert errors.__html__() == ''
