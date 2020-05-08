995c7c96017832dae768992801f6ee69ded82236
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-05-08 10:10:28 +0200

    Use path() instead of url() in docs and examples

--- examples/examples/urls.py
+++ examples/examples/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.urls import (
     include,
@@ -19,25 +18,25 @@ from examples.models import (
 from iommi.admin import Admin
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
 
     path('log_in/', views.log_in),
     path('log_out/', views.log_out),
 
-    url(r'^form_example_1/$', views.form_example_1, name='example_1'),
-    url(r'^form_example_2/$', views.form_example_2, name='example_2'),
-    url(r'^form_example_2b/$', Form.create(auto__model=Foo).as_view(), name='example_2b'),
-    url(r'^form_example_3/$', views.form_example_3, name='example_3'),
-    url(r'^form_example_4/$', views.form_example_4, name='example_4'),
-    url(r'^form_example_5/$', views.form_example_5, name='example_5'),
-    url(r'^form_kitchen/$', views.form_kitchen),
+    path('form_example_1/', views.form_example_1, name='example_1'),
+    path('form_example_2/', views.form_example_2, name='example_2'),
+    path('form_example_2b/', Form.create(auto__model=Foo).as_view(), name='example_2b'),
+    path('form_example_3/', views.form_example_3, name='example_3'),
+    path('form_example_4/', views.form_example_4, name='example_4'),
+    path('form_example_5/', views.form_example_5, name='example_5'),
+    path('form_kitchen/', views.form_kitchen),
 
-    url(r'^table_readme_example_1/$', views.table_readme_example_1, name='readme_example_1'),
-    url(r'^table_readme_example_2/$', views.table_readme_example_2, name='readme_example_2'),
-    url(r'^table_auto_example_1/$', views.table_auto_example_1, name='readme_example_1'),
-    url(r'^table_auto_example_2/$', views.table_auto_example_2, name='readme_example_2'),
-    url(r'^table_kitchen_sink/$', views.table_kitchen_sink, name='kitchen_sink'),
-    url(r'^table_as_view/$', Table(
+    path('table_readme_example_1/', views.table_readme_example_1, name='readme_example_1'),
+    path('table_readme_example_2/', views.table_readme_example_2, name='readme_example_2'),
+    path('table_auto_example_1/', views.table_auto_example_1, name='readme_example_1'),
+    path('table_auto_example_2/', views.table_auto_example_2, name='readme_example_2'),
+    path('table_kitchen_sink/', views.table_kitchen_sink, name='kitchen_sink'),
+    path('table_as_view/', Table(
         auto__model=TFoo,
         columns__a__bulk__include=True,
         bulk__actions__delete__include=True,
@@ -45,11 +44,11 @@ urlpatterns = [
         columns__a__extra_evaluated__report_name='A'
     ).as_view()),
 
-    url(r'^page_busy/$', views.page_busy, name='page_busy'),
-    url(r'^all_field_sorts/$', views.all_field_sorts, name='all_field_sorts'),
-    url(r'^all_column_sorts/$', views.all_column_sorts, name='all_column_sorts'),
+    path('page_busy/', views.page_busy, name='page_busy'),
+    path('all_field_sorts/', views.all_field_sorts, name='all_field_sorts'),
+    path('all_column_sorts/', views.all_column_sorts, name='all_column_sorts'),
 
-    url(r'^iommi-admin/', include(Admin.urls())),
+    path('iommi-admin/', include(Admin.urls())),
 
-    url(r'^menu_test/$', views.menu_test, name='menu_test'),
+    path('menu_test/', views.menu_test, name='menu_test'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
