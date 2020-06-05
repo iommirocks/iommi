1891d0b1707a1db9d52bebd1e876b1c9e18bbe2e
Johan Lübcke <johan.lubcke@trioptima.com>
2020-06-05 08:50:52 +0200

    Fix include to make None mean False (#20)

--- iommi/traversable.py
+++ iommi/traversable.py
@@ -175,7 +175,7 @@ class Traversable(RefinableObject):
                 **(parent._evaluate_parameters if parent is not None else {}),
                 **result.own_evaluate_parameters(),
             })
-            if include is False:
+            if not bool(include):
                 return None
         else:
             include = MISSING
