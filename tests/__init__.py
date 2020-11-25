d9a7ac8b30ad50c7c8600eddca325f5b1ff83cbf
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-11-25 15:06:01 +0100

    Fixed freezegun setup for pypy

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 import freezegun
 
-# Initialize freezegun to the current rolling time, to avoid freezegun being reinitialized which is expensive
-initialize_freezegun = freezegun.freeze_time(freezegun.api.real_datetime.now(), tick=True)
+# Initialize freezegun to avoid freezegun being reinitialized which is expensive
+initialize_freezegun = freezegun.freeze_time(datetime(2021, 1, 1))
 initialize_freezegun.start()
