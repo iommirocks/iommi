62218fcf1b530bcc06343a4b2dfae0aedbf5e4a8
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-12-04 19:12:52 +0100

    Fixed tests

--- iommi/profiling__tests.py
+++ iommi/profiling__tests.py
@@ -25,19 +25,19 @@ middleware = Middleware(lambda request: sentinel)
 def test_profiler_no_access():
     assert middleware(req('get')) is sentinel
     assert middleware(user_req('get')) is sentinel
-    assert middleware(user_req('get', prof='')) is sentinel
+    assert middleware(user_req('get', _iommi_prof='')) is sentinel
 
 
 def test_profiler_plain():
     middleware = Middleware(lambda request: sentinel)
-    assert 'white-space: nowrap' in middleware(staff_req('get', prof='')).content.decode()
+    assert 'white-space: nowrap' in middleware(staff_req('get', _iommi_prof='')).content.decode()
 
 
 def test_profiler_graph_error():
     old_sys_executable = sys.executable
     sys.executable = 'does_not_exist'
     with pytest.raises(Exception) as e:
-        middleware(staff_req('get', prof='', graph=''))
+        middleware(staff_req('get', _iommi_prof='', graph=''))
     sys.executable = old_sys_executable
 
     assert str(e.value) == 'gprof2dot not found. Please install it to use the graph feature.'
@@ -45,7 +45,7 @@ def test_profiler_graph_error():
 
 def test_profiler_graph_dot_present():
     if get_dot_path():
-        assert '<!DOCTYPE svg ' in middleware(staff_req('get', prof='', graph='')).content.decode()
+        assert '<!DOCTYPE svg ' in middleware(staff_req('get', _iommi_prof='', graph='')).content.decode()
 
 
 def test_profiler_graph_dot_not_present():
@@ -53,7 +53,7 @@ def test_profiler_graph_dot_not_present():
     orig = iommi.profiling._dot_search_paths[:]
     iommi.profiling._dot_search_paths[:] = ['does_not_exist']
 
-    response = middleware(staff_req('get', prof='', graph=''))
+    response = middleware(staff_req('get', _iommi_prof='', graph=''))
 
     iommi.profiling._dot_search_paths[:] = orig
 
