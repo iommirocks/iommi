a323d667ecf907049e9a339b46d60903f0d323d2
Anders Hovmöller <anders.hovmoller@dryft.se>
2021-02-04 16:05:52 +0100

    Updated to fontawesome 4.7

--- iommi/style_font_awesome_4.py
+++ iommi/style_font_awesome_4.py
@@ -1,11 +1,10 @@
-from iommi._web_compat import mark_safe
 from iommi.style import Style
 from iommi.fragment import html
 
 font_awesome_4 = Style(
     root__assets__icons=html.link(
         attrs__rel="stylesheet",
-        attrs__href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css",
+        attrs__href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     ),
     Column__shortcuts=dict(
         icon__extra=dict(
