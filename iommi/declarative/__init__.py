8138b76844298b882206dd3e8d28d50031c96c9e
Johan Lübcke <johan.lubcke@trioptima.com>
2022-10-25 10:17:18 +0200

    Added some type decoration from upstream tri-declarative

--- iommi/declarative/__init__.py
+++ iommi/declarative/__init__.py
@@ -1,9 +1,15 @@
 from copy import copy
+from typing import (
+    Type,
+    TypeVar,
+)
 
 from .util import (
     add_args_to_init_call,
     add_init_call_hook,
 )
+
+T = TypeVar("T")
 
 
 def declarative(member_class=None, parameter='members', add_init_kwargs=True, sort_key=None, is_member=None):
@@ -26,8 +32,8 @@ def declarative(member_class=None, parameter='members', add_init_kwargs=True, so
             "The @declarative decorator needs either a member_class parameter or an is_member check function (or both)"
         )
 
-    def decorator(class_to_decorate):
-        class DeclarativeMeta(class_to_decorate.__class__):
+    def decorator(class_to_decorate: Type[T]) -> Type[T]:
+        class DeclarativeMeta(class_to_decorate.__class__):  # type:ignore
             # noinspection PyTypeChecker,PyMethodParameters
             def __init__(cls, name, bases, dict_):
                 members = get_members(
