d22e3945ba28e7ab6a8940bae53ce9811fe3e0dd
Anders Hovmöller <anders.hovmoller@dryft.se>
2022-12-08 10:16:00 +0100

    Row grouping on EditTable now works

--- conftest.py
+++ conftest.py
@@ -2,6 +2,12 @@ from pathlib import Path
 
 import pytest
 
+from docs.models import (
+    Album,
+    Artist,
+    Track,
+)
+
 
 # pragma: no cover
 def pytest_runtest_setup(item):
@@ -29,3 +35,36 @@ def pytest_sessionstart(session):
     from iommi.docs import generate_api_docs_tests
 
     generate_api_docs_tests((Path(__file__).parent / 'docs').absolute())
+
+
+@pytest.fixture
+def artist():
+    return Artist.objects.create(name='Black Sabbath')
+
+
+@pytest.fixture
+def album(artist):
+    return Album.objects.create(name='Heaven & Hell', artist=artist, year=1980)
+
+
+@pytest.fixture
+def track(album):
+    return Track.objects.create(album=album, name='Neon Knights', index=1)
+
+
+@pytest.fixture
+def small_discography(artist):
+    return [
+        Album.objects.create(name='Heaven & Hell', artist=artist, year=1980),
+        Album.objects.create(name='Mob Rules', artist=artist, year=1981),
+    ]
+
+
+@pytest.fixture
+def medium_discography(artist):
+    ozzy = Artist.objects.create(name='Ozzy Osbourne')
+    return [
+        Album.objects.create(name='Heaven & Hell', artist=artist, year=1980),
+        Album.objects.create(name='Blizzard of Ozz', artist=ozzy, year=1980),
+        Album.objects.create(name='Mob Rules', artist=artist, year=1981),
+    ]
--- docs/conftest.py
+++ docs/conftest.py
@@ -1,44 +1,6 @@
 import pytest
-from docs.models import (
-    Album,
-    Artist,
-    Track,
-)
 
 
 @pytest.fixture(autouse=True)
 def docs_style(settings):
     settings.IOMMI_DEFAULT_STYLE = 'bootstrap_docs'
-
-
-@pytest.fixture
-def artist():
-    return Artist.objects.create(name='Black Sabbath')
-
-
-@pytest.fixture
-def album(artist):
-    return Album.objects.create(name='Heaven & Hell', artist=artist, year=1980)
-
-
-@pytest.fixture
-def track(album):
-    return Track.objects.create(album=album, name='Neon Knights', index=1)
-
-
-@pytest.fixture
-def small_discography(artist):
-    return [
-        Album.objects.create(name='Heaven & Hell', artist=artist, year=1980),
-        Album.objects.create(name='Mob Rules', artist=artist, year=1981),
-    ]
-
-
-@pytest.fixture
-def medium_discography(artist):
-    ozzy = Artist.objects.create(name='Ozzy Osbourne')
-    return [
-        Album.objects.create(name='Heaven & Hell', artist=artist, year=1980),
-        Album.objects.create(name='Blizzard of Ozz', artist=ozzy, year=1980),
-        Album.objects.create(name='Mob Rules', artist=artist, year=1981),
-    ]
