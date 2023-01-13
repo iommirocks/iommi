5db6ded5890a7cc448a5af27040d516119f580d6
Anders Hovmöller <anders.hovmoller@dryft.se>
2023-01-13 10:39:41 +0100

    Bootstrap style fix for grouped fields when using select2

--- iommi/style_bootstrap.py
+++ iommi/style_bootstrap.py
@@ -91,6 +91,11 @@ bootstrap_base = Style(
     FieldGroup=dict(
         tag='div',
         attrs__class={'form-row': True},
+        assets__field_group_select2_css=Asset('''
+        .form-group .select2-container {
+            display: block;
+        }
+        ''', tag='style'),
     ),
     Action=dict(
         shortcuts=dict(
