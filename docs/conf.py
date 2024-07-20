ad680421fd6b0ca438aa22ef8217b030862074b6
Anders Hovmöller <anders.hovmoller@dryft.se>
2024-07-21 01:04:31 +0200

    Prep for rtd deprecation of build time changes

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,15 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 from django import setup
