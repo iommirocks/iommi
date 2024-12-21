bd718064c3780d0fffccbfcb9b110080104dadd9
Johan Lübcke <johan@lubcke.se>
2024-12-21 19:35:36 +0100

    Cleanup

--- util.py
+++ util.py
@@ -18,7 +18,7 @@ def _read_version():
 
 def tag():
     version = _read_version()
-    errno = call(['git', 'tag', '--annotate', version, '--message', 'Version %s' % version])
+    errno = call(['git', 'tag', '--annotate', version, '--message', f'Version {version}'])
     if errno == 0:
         print("!!! Added tag for version %s" % version)
     raise SystemExit(errno)
@@ -26,16 +26,16 @@ def tag():
 
 def release_check():
     try:
-        tag = check_output(['git', 'describe', 'HEAD']).strip().decode('utf8')
+        tagged_version = check_output(['git', 'describe', 'HEAD']).decode().strip()
     except CalledProcessError:
-        tag = ''
+        tagged_version = ''
     version = _read_version()
 
-    if tag != version:
+    if tagged_version != version:
         print('### Missing %s tag on release' % version)
         raise SystemExit(1)
 
-    current_branch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode('utf8')
+    current_branch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode().strip()
     if current_branch != 'master':
         print('### Only release from master')
         raise SystemExit(1)
@@ -45,7 +45,9 @@ def release_check():
 
 def main():
     parser = argparse.ArgumentParser(description="Release helper script for iommi")
-    parser.add_argument('--version', action='version', version=_read_version(), help="Show program's version number and exit.")
+    parser.add_argument(
+        '--version', action='version', version=_read_version(), help="Show program's version number and exit."
+    )
     subparsers = parser.add_subparsers(title='subcommands', description='Available subcommands', dest='command')
     subparsers.add_parser('tag', help="Set version tag in git")
     subparsers.add_parser('release-check', help="Verify that tag and version info correspond.")
