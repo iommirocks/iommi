0446f12e7ad0df2eb804b91b8b6ed01dcb5101d4
Anders Hovmöller <anders.hovmoller@dryft.se>
2025-03-13 08:02:55 +0100

    Admin: set password fix (fixes #630)

--- iommi/admin.py
+++ iommi/admin.py
@@ -156,6 +156,8 @@ class Admin(Page):
     @read_config
     @with_defaults(
         apps__auth_user__include=True,
+        parts__edit_auth_user__fields__password__write_to_instance=lambda instance, value, **_: instance.set_password(value),
+        parts__edit_auth_user__fields__password__read_from_instance=lambda **_: '',
         apps__auth_group__include=True,
         parts__messages=Messages(),
         parts__list_auth_user=dict(
