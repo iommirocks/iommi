84e7bef3b3c92515b80c8870f0c86b9903d6ebcc
Johan Lübcke <johan.lubcke@prorenata.se>
2026-06-05 13:16:25 +0200

    Convert Sphinx docstring type declarations to real annotations

--- iommi/declarative/__init__.py
+++ iommi/declarative/__init__.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from copy import copy
 from typing import TypeVar
 
@@ -9,7 +10,7 @@ from .util import (
 T = TypeVar("T")
 
 
-def declarative(member_class=None, parameter='members', add_init_kwargs=True, sort_key=None, is_member=None):
+def declarative(member_class=None, parameter='members', add_init_kwargs=True, sort_key: Callable[[object], object] | None = None, is_member: Callable[[object], bool] | None = None):
     """
     Class decorator to enable classes to be defined in the style of django models.
     That is, @declarative classes will get an additional argument to constructor,
@@ -20,9 +21,6 @@ def declarative(member_class=None, parameter='members', add_init_kwargs=True, so
     :param str parameter: Name of constructor parameter to inject
     :param bool add_init_kwargs: If constructor parameter should be injected (Default: True)
     :param sort_key: Function to invoke on members to obtain ordering (Default is to use ordering from `creation_ordered`)
-
-    :type is_member: (object) -> bool
-    :type sort_key: (object) -> object
     """
     if member_class is None and is_member is None:
         raise TypeError(
@@ -64,32 +62,22 @@ def declarative(member_class=None, parameter='members', add_init_kwargs=True, so
     return decorator
 
 
-def set_declared(cls, value, parameter='members'):
-    """
-    :type cls: class
-    :type value: dict
-    :type parameter: str
-    """
-
+def set_declared(cls: type, value: dict, parameter: str = 'members'):
     setattr(cls, '_declarative_' + parameter, value)
 
 
-def get_declared(cls, parameter='members'):
+def get_declared(cls: type, parameter: str = 'members') -> dict:
     """
     Get the :code:`dict` value of the parameter collected by the :code:`@declarative` class decorator.
     This is the same value that would be submitted to the :code:`__init__` invocation in the :code:`members`
     argument (or another name if overridden by the :code:`parameter` specification)
-
-    :type cls: class
-    :type parameter: str
-    :rtype: dict
     """
 
     return getattr(cls, '_declarative_' + parameter, {})
 
 
 # noinspection PyIncorrectDocstring
-def get_members(cls, member_class=None, is_member=None, sort_key=None, _parameter=None):
+def get_members(cls, member_class=None, is_member: Callable[[object], bool] | None = None, sort_key: Callable[[object], object] | None = None, _parameter=None):
     """
     Collect all class level attributes matching the given criteria.
 
@@ -97,9 +85,6 @@ def get_members(cls, member_class=None, is_member=None, sort_key=None, _paramete
     :param class member_class: Class(es) to collect
     :param is_member: Function to determine if an object should be collected
     :param sort_key: Function to invoke on members to obtain ordering (Default is to use ordering from `creation_ordered`)
-
-    :type is_member: (object) -> bool
-    :type sort_key: (object) -> object
     """
     if member_class is None and is_member is None:
         raise TypeError("get_members either needs a member_class parameter or an is_member check function (or both)")
